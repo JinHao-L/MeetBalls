95aaff193e432b8ff4297081eb618bd1e6900358
Chong Wen Hao <58220142+8kdesign@users.noreply.github.com>
2021-10-26 22:09:44 +0800

    Update landing page

--- src/screens/LandingPage/LandingScreen.js
+++ src/screens/LandingPage/LandingScreen.js
@@ -98,32 +98,60 @@ export default function LandingScreen() {
             backgroundImage: `url(${BackgroundImage})`,
             backgroundPosition: 'center',
             backgroundSize: 'cover',
+            paddingBottom: 30,
+            paddingTop: 30,
+            paddingLeft: 20,
+            paddingRight: 20,
           }}
         >
-          <div className="Buffer--50px" />
-          <p
-            className="Text__header"
-            style={{ textAlign: 'center', color: 'white' }}
-          >
-            Key Features
-          </p>
-          <div className="Buffer--20px" />
           <Container>
+            <div
+              style={{
+                position: 'relative',
+                paddingBottom: '56.25%',
+                paddingLeft: 'auto',
+                paddingRight: 'auto',
+                textAlign: 'center',
+              }}
+            >
+              <iframe
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  position: 'absolute',
+                  borderRadius: 5,
+                  top: 0,
+                  left: 0,
+                }}
+                src={'https://www.youtube.com/embed/c8qQhg3S80s'}
+                frameBorder={0}
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+              />
+            </div>
+          </Container>
+        </div>
+        <Container>
+          <div className="Buffer--50px" />
+
+          <Container>
+            <p className="Text__header">Plan Meetings</p>
             <Row>
               <Col
                 sm={12}
                 md={6}
-                lg={4}
-                className="Container__padding--vertical-small"
+                lg={3}
+                className="Container__padding--vertical-medium"
               >
                 <Card style={{ height: '100%' }}>
-                  <Card.Img variant="top" src={AgendaImage} />
-                  <div className="Line--horizontal" />
                   <Card.Body>
-                    <Card.Title>Plan Your Meetings</Card.Title>
+                    <Card.Title>
+                      1. Create a new meeting or clone an existing one.
+                    </Card.Title>
                     <Card.Text>
-                      Add attendees and agenda before meeting, and send out mass
-                      emails to all participants.
+                      Link a Zoom meeting to MeetBalls. Clone an existing
+                      MeetBalls meeting to copy over participants and agenda
+                      items to the new meeting.
                     </Card.Text>
                   </Card.Body>
                 </Card>
@@ -131,17 +159,16 @@ export default function LandingScreen() {
               <Col
                 sm={12}
                 md={6}
-                lg={4}
-                className="Container__padding--vertical-small"
+                lg={3}
+                className="Container__padding--vertical-medium"
               >
                 <Card style={{ height: '100%' }}>
-                  <Card.Img variant="top" src={MeetingImage} />
-                  <div className="Line--horizontal" />
                   <Card.Body>
-                    <Card.Title>Stay Focused</Card.Title>
+                    <Card.Title>2. Add participants to the meeting.</Card.Title>
                     <Card.Text>
-                      MeetBalls helps you to mark attendance, and track timing
-                      for each agenda item during the meeting.
+                      Fill the participants list to make use of the various
+                      features that MeetBalls has, including attendance taking
+                      and sending of email invitations.
                     </Card.Text>
                   </Card.Body>
                 </Card>
@@ -149,34 +176,200 @@ export default function LandingScreen() {
               <Col
                 sm={12}
                 md={6}
-                lg={4}
-                className="Container__padding--vertical-small"
+                lg={3}
+                className="Container__padding--vertical-medium"
               >
                 <Card style={{ height: '100%' }}>
-                  <Card.Img variant="top" src={StatisticsImage} />
-                  <div className="Line--horizontal" />
                   <Card.Body>
-                    <Card.Title>Gain Insights</Card.Title>
+                    <Card.Title>3. Plan the agenda for the meeting.</Card.Title>
                     <Card.Text>
-                      After the meeting, view who was absent from the meeting,
-                      as well as how long each item took.
+                      Add agenda items to the agenda to view them during the
+                      meeting. Attach files to agenda items and link
+                      participants to agenda items.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      4. Send out invites to all participants.
+                    </Card.Title>
+                    <Card.Text>
+                      Inviting pariticipants to the meeting is just 2 clicks
+                      away. Participants will receive personalized email with
+                      their very own link to access the meeting.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <div className="Buffer--20px" />
+            <Row>
+              <p className="Text__header">Track Meetings</p>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>1. Take attendance automatically.</Card.Title>
+                    <Card.Text>
+                      Thanks to integration with Zoom, MeetBalls is able to
+                      automatically update the attendance list when the
+                      participant enters the Zoom meeting.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      2. Be alerted when you exceed the time limit.
+                    </Card.Title>
+                    <Card.Text>
+                      Stay on track of your schedule with MeetBalls. When time
+                      is up for an item, the bell will go off to alert you to
+                      move on to the next item.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      3. Analyse and learn from past meeting.
+                    </Card.Title>
+                    <Card.Text>
+                      View meeting statistics, attendance and duration of each
+                      agenda item from past meetings.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      4. Mass email to participants after the meeting.
+                    </Card.Title>
+                    <Card.Text>
+                      Need to send out meeting minutes? Include all participants
+                      in your email with just a click of a button from any
+                      completed meeting.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            <div className="Buffer--20px" />
+            <Row>
+              <p className="Text__header">Join Meetings</p>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      1. Upload files for your presentations.
+                    </Card.Title>
+                    <Card.Text>
+                      View the items you are presenting during the meeting and
+                      attach relevant files or link via the invitation link sent
+                      to your email.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      2. Suggests topics for meeting agenda.
+                    </Card.Title>
+                    <Card.Text>
+                      Have something to discuss or present during the meeting?
+                      Leave a suggestion and the meeting host will be able to
+                      see it.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      3. Access files for each presentation.
+                    </Card.Title>
+                    <Card.Text>
+                      Before or during the meeting, you can gain access to the
+                      files the presenter has uploaded from the agenda list.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col
+                sm={12}
+                md={6}
+                lg={3}
+                className="Container__padding--vertical-medium"
+              >
+                <Card style={{ height: '100%' }}>
+                  <Card.Body>
+                    <Card.Title>
+                      4. Get an estimate of when the meeting will end.
+                    </Card.Title>
+                    <Card.Text>
+                      Your time is precious. MeetBalls provides an estimated end
+                      time of the meeting based on the items remaining in the
+                      agenda.
                     </Card.Text>
                   </Card.Body>
                 </Card>
               </Col>
             </Row>
           </Container>
-          <div className="Buffer--20px" />
-          <div className="Container__row--center">
-            <Button
-              variant="secondary"
-              onClick={() => history.push('/documentation')}
-            >
-              Learn More
-            </Button>
-          </div>
+
           <div className="Buffer--50px" />
-        </div>
+        </Container>
       </Container>
       <AppFooter />
     </div>
